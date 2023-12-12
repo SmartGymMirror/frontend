18d4864b1272419802e54d384178e7dcfede0950
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2023-12-12 16:59:34 +0100

    bug weather

--- src/components/WeatherScreen.jsx
+++ src/components/WeatherScreen.jsx
@@ -10,10 +10,13 @@ const WeatherScreen = () => {
       : 'Madrid'
   )
 
+  const [loadingWeatherInfo, setLoadingWeatherInfo] = useState(true)
+
   const [weatherInfo, setWeatherInfo] = useState('')
 
   useEffect(() => {
     const fetchWeather = async () => {
+      setLoadingWeatherInfo(true)
       try {
         const res = await fetch(
           `https://django-weather-api.vercel.app/api/weather/?localizacion=${city}`
@@ -21,7 +24,7 @@ const WeatherScreen = () => {
         if (res.ok) {
           const data = await res.json()
           setWeatherInfo(data)
-          console.log(data)
+          setLoadingWeatherInfo(false)
         } else {
           throw new Error('Failed to fetch')
         }
@@ -38,18 +41,64 @@ const WeatherScreen = () => {
       <h1>Pronóstico meteorológico</h1>
       <h2>Ciudad actual: {city}</h2>
       <div className="pronostico-meteorologico">
-        <div className="left-data">
-          <p>Temperatura actual: {weatherInfo?.main?.temp ? `${weatherInfo.main.temp} ºC` : 'Buscando...'}</p>
-          <p>Temperatura máxima: {weatherInfo?.main?.temp_max ? `${weatherInfo.main.temp_max} ºC` : 'Buscando...'}</p>
-          <p>Temperatura mínima: {weatherInfo?.main?.temp_min ? `${weatherInfo.main.temp_min} ºC` : 'Buscando...'}</p>
-          <p>Sensación térmica: {weatherInfo?.main?.feels_like ? `${weatherInfo.main.feels_like} ºC` : 'Buscando...'}</p>
-        </div>
-        <div className="right-data">
-          <p>Descripción: {weatherInfo?.weather[0]?.description ? weatherInfo.weather[0].description : 'Buscando...'}</p>
-          <p>Humedad: {weatherInfo?.main?.humidity ? `${weatherInfo.main.humidity} %` : 'Buscando...'}</p>
-          <p>Presión: {weatherInfo?.main?.pressure ? `${weatherInfo.main.pressure} hPa` : 'Buscando...'}</p>
-          <p>Viento: {weatherInfo?.wind?.speed ? `${weatherInfo.wind.speed} m/s` : 'Buscando...'}</p>
-        </div>
+        {loadingWeatherInfo ? (
+          'Cargando...'
+        ) : (
+          <>
+            <div className="left-data">
+              <p>
+                Temperatura actual:{' '}
+                {weatherInfo?.main?.temp
+                  ? `${weatherInfo.main.temp} ºC`
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Temperatura máxima:{' '}
+                {weatherInfo?.main?.temp_max
+                  ? `${weatherInfo.main.temp_max} ºC`
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Temperatura mínima:{' '}
+                {weatherInfo?.main?.temp_min
+                  ? `${weatherInfo.main.temp_min} ºC`
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Sensación térmica:{' '}
+                {weatherInfo?.main?.feels_like
+                  ? `${weatherInfo.main.feels_like} ºC`
+                  : 'Buscando...'}
+              </p>
+            </div>
+            <div className="right-data">
+              <p>
+                Descripción:{' '}
+                {weatherInfo?.weather[0]?.description
+                  ? weatherInfo.weather[0].description
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Humedad:{' '}
+                {weatherInfo?.main?.humidity
+                  ? `${weatherInfo.main.humidity} %`
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Presión:{' '}
+                {weatherInfo?.main?.pressure
+                  ? `${weatherInfo.main.pressure} hPa`
+                  : 'Buscando...'}
+              </p>
+              <p>
+                Viento:{' '}
+                {weatherInfo?.wind?.speed
+                  ? `${weatherInfo.wind.speed} m/s`
+                  : 'Buscando...'}
+              </p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
