df0c7d1c2ecab1f23272a5160479a4e74a441f1d
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2023-12-17 19:23:56 +0100

    Add 20s interaction timeout and navigation redirect

--- src/app/mirror/page.jsx
+++ src/app/mirror/page.jsx
@@ -1,17 +1,56 @@
 'use client'
 
+import '@/styles/mirror.css'
 import ChartScreen from '@/components/ChartScreen'
 import SettingsScreen from '@/components/SettingsScreen'
 import WeatherScreen from '@/components/WeatherScreen'
 import Widget from '@/components/Widget'
-import '@/styles/mirror.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { useRouter } from 'next/navigation'
+
+const SUSPENSION_DELAY = 20000
 
 const Mirror = () => {
+  const router = useRouter()
+  const redirectTimer = useRef(null)
+
+  const redirectToStandby = () => {
+    router.push('/')
+  }
+
+  const resetTimer = () => {
+    console.log('reset timer')
+    if (redirectTimer.current) {
+      clearTimeout(redirectTimer.current)
+    }
+    redirectTimer.current = setTimeout(redirectToStandby, SUSPENSION_DELAY)
+  }
+
+  const handleInteraction = () => {
+    resetTimer()
+  }
+
+  useEffect(() => {
+    resetTimer()
+
+      window.addEventListener('ontouchstart', handleInteraction)
+      window.addEventListener('keydown', handleInteraction)
+      window.addEventListener('click', handleInteraction)
+
+    return () => {
+      if (redirectTimer.current) {
+        clearTimeout(redirectTimer.current)
+      }
+      window.removeEventListener('ontouchstart', handleInteraction)
+      window.removeEventListener('keydown', handleInteraction)
+      window.removeEventListener('click', handleInteraction)
+    }
+  }, [router])
+
   const widgets = {
-    weather: { src: '/icons/weather.svg', component: WeatherScreen},
-    progress: { src: '/icons/chart.svg', component: ChartScreen},
-    settings: { src: '/icons/settings.svg', component: SettingsScreen},
+    weather: { src: '/icons/weather.svg', component: WeatherScreen },
+    progress: { src: '/icons/chart.svg', component: ChartScreen },
+    settings: { src: '/icons/settings.svg', component: SettingsScreen },
   }
 
   const [activeWidget, setActiveWidget] = useState(widgets.weather)
@@ -19,7 +58,7 @@ const Mirror = () => {
   const renderActiveScreen = () => {
     const ScreenComponent = activeWidget.component
     return (
-      <ScreenComponent/>
+      <ScreenComponent />
     )
   }
 
@@ -29,9 +68,9 @@ const Mirror = () => {
         {renderActiveScreen()}
       </div>
       <div className="widgets-container">
-        <Widget widget={widgets.weather} activeWidget={setActiveWidget}/>
-        <Widget widget={widgets.progress} activeWidget={setActiveWidget}/>
-        <Widget widget={widgets.settings} activeWidget={setActiveWidget}/>
+        <Widget widget={widgets.weather} activeWidget={setActiveWidget} />
+        <Widget widget={widgets.progress} activeWidget={setActiveWidget} />
+        <Widget widget={widgets.settings} activeWidget={setActiveWidget} />
       </div>
     </div>
   )
